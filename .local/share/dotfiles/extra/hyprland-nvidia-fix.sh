106c70e66679e89842505ebf55f4983a7af0fa01
Harzo <harzo@hhar.cc>
2023-08-24 17:50:25 +0200

    Dotfiles: Remove comments

--- .local/share/dotfiles/extra/hyprland-nvidia-fix.sh
+++ .local/share/dotfiles/extra/hyprland-nvidia-fix.sh
@@ -6,12 +6,5 @@ root_guard
 
 echo "options nvidia NVreg_PreserveVideoMemoryAllocations=1 NVreg_TemporaryFilePath=/path/to/tmp-nvidia" > /etc/modprobe.d/nvidia-power-management.conf
 
-#install -m 755 "${CURDIR}/../files/scripts/suspend-hyprland" /usr/local/bin
-#install -m 644 "${CURDIR}/../files/services/hyprland-suspend.service" /usr/lib/systemd/system
-#install -m 644 "${CURDIR}/../files/services/hyprland-resume.service" /usr/lib/systemd/system
-#
-#systemctl daemon-reload
-#systemctl enable hyprland-suspend.service
-#systemctl enable hyprland-resume.service
 systemctl enable nvidia-suspend
 systemctl enable nvidia-hibernate
