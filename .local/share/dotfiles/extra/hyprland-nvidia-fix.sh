bab7883ad277b3b2bcc8405f35ea4fca0a917728
Harzo <harzo@hhar.cc>
2023-08-24 17:53:45 +0200

    Dotfiles: Add pacman hook to nvidia fix

--- .local/share/dotfiles/extra/hyprland-nvidia-fix.sh
+++ .local/share/dotfiles/extra/hyprland-nvidia-fix.sh
@@ -6,5 +6,7 @@ root_guard
 
 echo "options nvidia NVreg_PreserveVideoMemoryAllocations=1 NVreg_TemporaryFilePath=/path/to/tmp-nvidia" > /etc/modprobe.d/nvidia-power-management.conf
 
+install -m 644 "${CURDIR}/../files/hooks/nvidia.hook" /etc/pacman.d/hooks/nvidia.hook
+
 systemctl enable nvidia-suspend
 systemctl enable nvidia-hibernate
